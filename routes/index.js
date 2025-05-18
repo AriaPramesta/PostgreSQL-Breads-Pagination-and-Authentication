db5eada2785a1385aea83ae446ee0354e09913ad
AriaPramesta <ariaapramesta@gmail.com>
2025-05-18 11:50:50 +0700

    update: create sign in and sign up ui using bootstrap template, and using nodemon for development

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,14 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+module.exports = function (db) {
+  router.get("/", function (req, res, next) {
+    res.render("login");
+  });
 
-module.exports = router;
+  router.get("/register", function (req, res, next) {
+    res.render("register");
+  });
+
+  return router;
+};
